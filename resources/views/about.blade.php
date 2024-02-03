2e58fd6459644aaa1c4617e0bc1bd07a69ec2690
Shila Afifah <shilanurafifah@gmail.com>
2024-02-03 12:25:36 +0700

    Second Commit

---
+++ resources/views/about.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
+    <title>About | Shila</title>
+</head>
+<body>
+    <h3>Name: Shila Agni Nur Afifah <br>
+        College: Jember State Polytechnic <br>
+        Email: shilanurafifah@gmail.com 
+    </h3>
+    <img src="img/shila.jpg" alt="Photo Shila Afifah" width="200">
+</body>
+</html>
---
+++ resources/views/home.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
+    <title>Home | Shila</title>
+</head>
+<body>
+    <h2>Hello, this is Shila Agni Nur Afifah's home page website</h2>
+</body>
+</html>
